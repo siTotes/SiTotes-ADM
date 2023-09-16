e1b5f98ffa0116ab8e439a6628a3f0a267cfb76a
siTotes <timeminecraft.1140@gmail.com>
2023-09-16 19:17:06 +0700

    m

--- gg.js
+++
@@ -1,5 +0,0 @@
-const objekJSON = [{ _id: 'ak9dkksk' }];
-const jsonString = JSON.stringify(objekJSON);
-const panjangString = jsonString.length;
-
-console.log(`Panjang string JSON adalah ${panjangString} karakter.`);
