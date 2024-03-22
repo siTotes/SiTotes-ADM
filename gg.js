76acc9224b54cb9195eaa85781bfef93dc6758ad
siTotes <timeminecraft.1140@gmail.com>
2024-03-22 12:22:33 +0700

    m

--- gg.js
+++ gg.js
@@ -1,10 +1,66 @@
-const { TiktokDownloader } = require("@tobyg74/tiktok-api-dl")
+const dataRekayasa = [
+  {
+    nama1: "saiful",
+    nama2: "bila",
+    outPersen: 60
+  },
+  {
+    nama1: "anam",
+    nama2: "bila",
+    outPersen: 60
+  },
+  {
+    nama1: "yoga",
+    nama2: "monic",
+    outPersen: 60
+  },
+  {
+    nama1: "yoga",
+    nama2: "onichiwa",
+    outPersen: 80
+  },
+  {
+    nama1: "yoga",
+    nama2: "oniciwa",
+    outPersen: 80
+  },
+  {
+    nama1: "yoga",
+    nama2: "monika",
+    outPersen: 60
+  },
+  {
+    nama1: "yoga",
+    nama2: "monik",
+    outPersen: 60
+  },
+  {
+    nama1: "akbar",
+    nama2: "adist",
+    outPersen: 60
+  },
+  {
+    nama1: "akbar",
+    nama2: "adis",
+    outPersen: 60
+  },
+  {
+    nama1: "maul",
+    nama2: "adis",
+    outPersen: 60
+  }
+]
 
-// const tiktok_url = "https://vt.tiktok.com/ZSF8yy3na/" //gambar
-const tiktok_url = "https://vt.tiktok.com/ZSFamLSCN/" //video
+for(let i = 0; i<dataRekayasa.length; i++){
+    if (text.toLowerCase().includes(dataRekayasa[i].nama1) && text.toLowerCase().includes(dataRekayasa[i].nama2)) {
+        let rating = Math.floor(Math.random() * 11) + dataRekayasa[i].outPersen;
+        await reply(`Rate: ${text} (${rating}%)`)
+    } else {
+        let rating = Math.floor(Math.random() * 101)
+        await reply(`Rate: ${text} (${rating}%)`)
+    }
+}
 
-TiktokDownloader(tiktok_url, {
-  version: "v3" //  version: "v1" | "v2" | "v3"
-}).then((result) => {
-  console.log(result)
-})
+
+
+
--- src/commands/openai-gpt.js
+++ src/commands/openai-gpt.js
@@ -65,7 +65,74 @@ module.exports = onic = async (onic, m, command, mek) => {
             break
             case 'rate':{
                 await react('⌛')
-                await reply(`*Rate:* ${text} (${Math.floor(Math.random() * 101)}%)`);
+                // await reply(`*Rate:* ${text} (${Math.floor(Math.random() * 101)}%)`);
+const dataRekayasa = [
+  {
+    nama1: "saiful",
+    nama2: "bila",
+    outPersen: 60
+  },
+  {
+    nama1: "anam",
+    nama2: "bila",
+    outPersen: 60
+  },
+  {
+    nama1: "yoga",
+    nama2: "monic",
+    outPersen: 60
+  },
+  {
+    nama1: "yoga",
+    nama2: "onichiwa",
+    outPersen: 80
+  },
+  {
+    nama1: "yoga",
+    nama2: "oniciwa",
+    outPersen: 80
+  },
+  {
+    nama1: "yoga",
+    nama2: "monika",
+    outPersen: 60
+  },
+  {
+    nama1: "yoga",
+    nama2: "monik",
+    outPersen: 60
+  },
+  {
+    nama1: "akbar",
+    nama2: "adist",
+    outPersen: 60
+  },
+  {
+    nama1: "akbar",
+    nama2: "adis",
+    outPersen: 60
+  },
+  {
+    nama1: "maul",
+    nama2: "adis",
+    outPersen: 60
+  }
+]
+
+for(let i = 0; i<dataRekayasa.length; i++){
+    if (text.toLowerCase().includes(dataRekayasa[i].nama1) && text.toLowerCase().includes(dataRekayasa[i].nama2)) {
+        let rating = Math.floor(Math.random() * 11) + dataRekayasa[i].outPersen;
+        await reply(`Rate: ${text} (${rating}%)`)
+    } else {
+        let rating = Math.floor(Math.random() * 101)
+        await reply(`Rate: ${text} (${rating}%)`)
+    }
+}
+
+
+
+
+
                 await react('✅')
             }
             break
